1975e597a4491a498e54252eee8362bf0aa901ba
alfan.fathurahman@solusione.id <alfan.fathurahman@solusione.id>
2025-05-03 08:54:53 +0700

    update callbacks auth

--- actions/login.ts
+++ actions/login.ts
@@ -1,12 +1,35 @@
+"use server";
+
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schemas";
+import { AuthError } from "next-auth";
 import { z } from "zod";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
 
-  if (!validatedFields) {
+  if (!validatedFields.success) {
     return { error: "Invalid Fields!" };
   }
 
-  return { success: "Email Sent!" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch ((error as any).type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+    throw error;
+  }
 };
--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,26 @@
-import GitHub from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
 
-export default { providers: [GitHub] } satisfies NextAuthConfig;
+import { LoginSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
+import bcrypt from "bcryptjs";
+
+export default {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validateFields = LoginSchema.safeParse(credentials);
+        if (validateFields.success) {
+          const { email, password } = validateFields.data;
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
